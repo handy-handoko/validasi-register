a751010c78034baeef89703f2610cbb2f59e423e
handy handoko <phan.handy.handoko@gmail.com>
2017-09-06 07:15:48 +0800

    get data yang sudah diinput dari database

--- validate.php
+++ validate.php
@@ -1,3 +1,12 @@
 <?php 
 //panggil file koneksi.php agar bisa terhubung ke database
 include 'koneksi.php';
+
+//ambil username yang akan divalidasi. 
+$uname = $_GET['username'];
+
+//buat perintah sql untuk query data username yang diinput user
+$sql = "SELECT `user_id` FROM tb_validasi WHERE `username` = ?";
+$stmt = $dbCon->prepare($sql);
+//tambahkan parameter $uname ke dalam statement. 
+$stmt->execute(array($uname));
